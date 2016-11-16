e6183347c9f997322620d8246fdaf92b407c739c
andy-dufour <adufour@chef.io>
2016-11-15 20:47:42 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# AppStackA
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'AppStackA'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures AppStackA'
+long_description 'Installs/Configures AppStackA'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/AppStackA/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/AppStackA' if respond_to?(:source_url)
